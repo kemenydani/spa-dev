ad09dc56a907d3a9cb8e5a8eb76918eb3eb9df9b
kemenydani <kemenydani93@gmail.com>
2018-02-01 16:27:42 +0100

    progress

--- resources/spa-admin/components/views/main.vue
+++ resources/spa-admin/components/views/main.vue
@@ -94,42 +94,25 @@
 						<v-icon dark language>message</v-icon>
 					</v-btn>
 					
-					<v-card>
-						<v-list>
-							<v-list-tile avatar>
-								<v-list-tile-avatar>
-									<img src="/static/doc-images/john.jpg" alt="John">
-								</v-list-tile-avatar>
-								<v-list-tile-content>
-									<v-list-tile-title>John Leider</v-list-tile-title>
-									<v-list-tile-sub-title>Founder of Vuetify.js</v-list-tile-sub-title>
-								</v-list-tile-content>
-								<v-list-tile-action>
-									<v-btn
-											icon
-											:class="fav ? 'red--text' : ''"
-											@click="fav = !fav"
-									>
-										<v-icon>favorite</v-icon>
-									</v-btn>
-								</v-list-tile-action>
-							</v-list-tile>
+					<v-card style="max-width: 400px">
+						
+						<v-list three-line>
+							<template v-for="item in items">
+								<v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+								<v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+								<v-list-tile avatar v-else v-bind:key="item.title" @click="">
+									<v-list-tile-avatar>
+										<img v-bind:src="item.avatar"/>
+									</v-list-tile-avatar>
+									<v-list-tile-content>
+										<v-list-tile-title v-html="item.title"></v-list-tile-title>
+										<v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+									</v-list-tile-content>
+								</v-list-tile>
+							</template>
 						</v-list>
-						<v-divider></v-divider>
-						<v-list>
-							<v-list-tile>
-								<v-list-tile-action>
-									<v-switch v-model="message" color="purple"></v-switch>
-								</v-list-tile-action>
-								<v-list-tile-title>Enable messages</v-list-tile-title>
-							</v-list-tile>
-							<v-list-tile>
-								<v-list-tile-action>
-									<v-switch v-model="hints" color="purple"></v-switch>
-								</v-list-tile-action>
-								<v-list-tile-title>Enable hints</v-list-tile-title>
-							</v-list-tile>
-						</v-list>
+						
+						
 						<v-card-actions>
 							<v-spacer></v-spacer>
 							<v-btn flat @click="menu = false">Cancel</v-btn>
@@ -180,6 +163,19 @@
 		},
 		data () {
 			return {
+				
+				items: [
+					{ header: 'Today' },
+					{ avatar: 'https://scontent.ftxl1-1.fna.fbcdn.net/v/t1.0-9/11261407_1024234314285914_3210054362884355031_n.jpg?oh=d65cd194ad3fe1a3b84ca25383227466&oe=5ADEF36F', title: 'Brunch this weekend?', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+					{ divider: true, inset: true },
+					{ avatar: 'https://scontent.ftxl1-1.fna.fbcdn.net/v/t1.0-9/11261407_1024234314285914_3210054362884355031_n.jpg?oh=d65cd194ad3fe1a3b84ca25383227466&oe=5ADEF36F', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend." },
+					{ divider: true, inset: true },
+					{ avatar: 'https://scontent.ftxl1-1.fna.fbcdn.net/v/t1.0-9/11261407_1024234314285914_3210054362884355031_n.jpg?oh=d65cd194ad3fe1a3b84ca25383227466&oe=5ADEF36F', title: 'Oui oui', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?" },
+					{ divider: true, inset: true },
+					{ avatar: 'https://scontent.ftxl1-1.fna.fbcdn.net/v/t1.0-9/11261407_1024234314285914_3210054362884355031_n.jpg?oh=d65cd194ad3fe1a3b84ca25383227466&oe=5ADEF36F', title: 'Birthday gift', subtitle: "<span class='grey--text text--darken-2'>Trevor Hansen</span> — Have any ideas about what we should get Heidi for her birthday?" },
+					{ divider: true, inset: true },
+					{ avatar: 'https://scontent.ftxl1-1.fna.fbcdn.net/v/t1.0-9/11261407_1024234314285914_3210054362884355031_n.jpg?oh=d65cd194ad3fe1a3b84ca25383227466&oe=5ADEF36F', title: 'Recipe to try', subtitle: "<span class='grey--text text--darken-2'>Britta Holt</span> — We should eat this: Grate, Squash, Corn, and tomatillo Tacos." },
+				],
 				
 				fav: true,
 				menu: false,
