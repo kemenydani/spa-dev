c0f02628a675776eccdb5c19d4407aaf5eb158d9
Dani <kemenydani93@gmail.com>
2018-03-04 20:02:48 +0100

    progress

--- resources/spa-public/components/views/home/view.vue
+++ resources/spa-public/components/views/home/view.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="container hc">
-		home
+		<div class="home-grid">
+			<div class="grid-item">item 1</div>
+			<div class="grid-item">item 2</div>
+			<div class="grid-item">item 3</div>
+			<div class="grid-item">item 4</div>
+			<div class="grid-item">item 5</div>
+			<div class="grid-item">item 6</div>
+		</div>
 	</div>
 </template>
 
 <script>
-	
 	export default {
 		name: 'home-view',
 		data() {
@@ -17,5 +23,14 @@
 </script>
 
 <style lang="scss">
-
+	.home-grid {
+		width: 100%;
+		background: white;
+		display: grid;
+		
+		.grid-item {
+			background: black;
+			color: white;
+		}
+	}
 </style>
