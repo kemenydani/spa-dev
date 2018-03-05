f7d9c54f48bcc209fe5aa8e8a88ee970a26c082d
kemenydani <kemenydani93@gmail.com>
2018-03-05 12:57:33 +0100

    progress

---
+++ resources/spa-public/components/widgets/article-item.vue
@@ -0,0 +1,89 @@
+<template>
+	
+	<article class="article-item">
+	
+		<section class="header" :style="{ backgroundImage: 'url(' + model.image + ')' }"></section>
+		
+		<section class="content">
+			
+			<div class="info"></div>
+			
+			<h2 v-if="model.title" class="title"> {{ model.title }} </h2>
+			
+			<p v-if="model.teaser" class="teaser">{{ model.teaser }} </p>
+			
+		</section>
+		
+		<div class="footer"></div>
+		
+	</article>
+	
+</template>
+
+<script>
+	
+	export default {
+		name: "article-item",
+		props: {
+			headline: {
+				type: Boolean,
+				required: false,
+				default: () => false
+			},
+			teaser: {
+				type: Boolean,
+				required: false,
+				default: () => false
+			},
+			model: {
+				required: true,
+				type: Object,
+				default: () => {
+					return {
+						title: '',
+						teaser: '',
+						image: '',
+						datePosted: '',
+					}
+				},
+			},
+		},
+		data() {
+
+		},
+		methods: {
+		
+		},
+		beforeCreate(){
+
+		}
+	}
+	
+</script>
+
+<style lang="scss" scoped>
+	
+	.article-item {
+		
+		.header {
+			width: 100%;
+			height: 170px;
+		}
+		
+		.content {
+			
+			.info {
+			
+			}
+			
+			.title {
+			
+			}
+			
+			.teaser {
+			
+			}
+		}
+	}
+	
+</style>
