238a87fdf0cb13c9cc25e6dcec51af1886572bb4
kemenydani <kemenydani93@gmail.com>
2018-03-06 13:37:40 +0100

    progress

--- resources/spa-public/components/views/home/view.vue
+++ resources/spa-public/components/views/home/view.vue
@@ -50,64 +50,6 @@
 
 <style lang="scss">
 	
-	.home-grid
-	{
-		padding-top: 10px;
-		padding-bottom: 10px;
-		
-		@supports ( display: flex )
-		{
-			display: flex;
-			flex-wrap: wrap;
-			justify-content: space-between;
-		}
-		
-		@supports ( display: grid )
-		{
-			display: grid;
-			grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-			grid-template-rows: auto;
-			grid-gap: 20px;
-			
-			padding-top: 20px; /* override */
-			padding-bottom: 20px; /* override */
-			
-			:nth-child(1) { grid-column: span 2; }
-			:nth-child(7) { grid-column: span 2; }
-		}
-		
-	}
-	.grid-item
-	{
-		position: relative;
-		overflow: hidden;
-		
-		margin: 10px;
-		
-		float: left;
-		
-		width: calc(33.3333% - 20px);
-		max-width: calc(33.3333% - 20px);
-		
-		background: white;
-		
-		&:nth-child(1) { width: calc(66.6666% - 20px); max-width: calc(66.6666% - 20px) }
-		&:nth-child(2) { width: calc(33.3333% - 20px); max-width: calc(33.3333% - 20px) }
-		
-		&:nth-child(6) { width: calc(33.3333% - 20px); max-width: calc(33.3333% - 20px) }
-		&:nth-child(7) { width: calc(66.6666% - 20px); max-width: calc(66.6666% - 20px) }
-		
-		@supports ( display: flex ) { float: none; }
-		
-		@supports ( display: grid )
-		{
-			float: none;
-			width: 100%; /* override */
-			min-width: 100%;
-			max-width: 100%; /* override */
-			margin: 0;
-		}
-		
-	}
+	
 	
 </style>
