7f6ec77b1f329976a843c7392276d5e45e234161
kemenydani <kemenydani93@gmail.com>
2018-03-06 13:07:30 +0100

    progress

--- resources/spa-public/components/views/home/view.vue
+++ resources/spa-public/components/views/home/view.vue
@@ -1,7 +1,18 @@
 <template>
 	<div class="page-view">
 		<div class="container hc full-height">
-			<div class="home-first-row">
+			
+			<div class="home-grid">
+				<div class="grid-item">
+					<article-item
+							class="side-image" :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
+				</div>
+				<div class="grid-item">
+					<article-item :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
+				</div>
+				<div class="grid-item">
+					<article-item :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
+				</div>
 				<div class="grid-item">
 					<article-item :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
 				</div>
@@ -11,14 +22,8 @@
 				<div class="grid-item">
 					<article-item :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
 				</div>
-			</div>
-		
-			<div class="home-second-row">
 				<div class="grid-item">
-					<article-item :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
-				</div>
-				<div class="grid-item">
-					<article-item :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
+					<article-item class="side-image-rev" :model="{ title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', teaser: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', image: 'https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg' }"> </article-item>
 				</div>
 			</div>
 			
@@ -44,27 +49,65 @@
 </script>
 
 <style lang="scss">
-	.home-first-row {
-		@supports ( display: grid ) {
+	
+	.home-grid
+	{
+		padding-top: 10px;
+		padding-bottom: 10px;
+		
+		@supports ( display: flex )
+		{
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-between;
+		}
+		
+		@supports ( display: grid )
+		{
 			display: grid;
 			grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
 			grid-template-rows: auto;
 			grid-gap: 20px;
+			
+			padding-top: 20px; /* override */
+			padding-bottom: 20px; /* override */
+			
+			:nth-child(1) { grid-column: span 2; }
+			:nth-child(7) { grid-column: span 2; }
 		}
+		
 	}
-	
-	.home-second-row {
-		@supports ( display: grid ) {
-			display: grid;
-			grid-template-columns: minmax(600px, 2fr) minmax(300px, 1fr);
-			grid-template-rows: auto;
-			grid-gap: 20px;
-		}
-	}
-	
 	.grid-item
 	{
-			background: white;
+		position: relative;
+		overflow: hidden;
+		
+		margin: 10px;
+		
+		float: left;
+		
+		width: calc(33.3333% - 20px);
+		max-width: calc(33.3333% - 20px);
+		
+		background: white;
+		
+		&:nth-child(1) { width: calc(66.6666% - 20px); max-width: calc(66.6666% - 20px) }
+		&:nth-child(2) { width: calc(33.3333% - 20px); max-width: calc(33.3333% - 20px) }
+		
+		&:nth-child(6) { width: calc(33.3333% - 20px); max-width: calc(33.3333% - 20px) }
+		&:nth-child(7) { width: calc(66.6666% - 20px); max-width: calc(66.6666% - 20px) }
+		
+		@supports ( display: flex ) { float: none; }
+		
+		@supports ( display: grid )
+		{
+			float: none;
+			width: 100%; /* override */
+			min-width: 100%;
+			max-width: 100%; /* override */
+			margin: 0;
+		}
+		
 	}
 	
 </style>
--- resources/spa-public/components/widgets/article-item.vue
+++ resources/spa-public/components/widgets/article-item.vue
@@ -2,7 +2,9 @@
 	
 	<article class="article-item">
 	
-		<section class="header" :style="{ backgroundImage: 'url(' + model.image + ')' }"></section>
+		<section class="header">
+			<div class="image" :style="{ backgroundImage: 'url(' + model.image + ')' }"></div>
+		</section>
 		
 		<section class="content">
 			
@@ -63,22 +65,103 @@
 
 <style lang="scss" scoped>
 	
-	.article-item {
+	.article-item
+	{
+		overflow: hidden;
+		position: relative;
+		height: 100%;
+		
+		border-radius: 4px;
 		
 		.header {
+			overflow: hidden;
+			position: relative;
 			width: 100%;
-			height: 170px;
-			background-repeat: no-repeat;
+			min-height: 200px;
+			border-bottom: 4px solid orange;
+			
+			.image {
+				position: absolute;
+				width: 100%;
+				height: 100%;
+				
+				background-repeat: no-repeat;
+				background-position: center center;
+				background-size: cover;
+				
+				transition: all 1s ease;
+				-moz-transition: all 1s ease;
+				-ms-transition: all 1s ease;
+				-webkit-transition: all 1s ease;
+				-o-transition: all 1s ease;
+				
+				&:hover {
+					transform: scale(1.1);
+					-moz-transform: scale(1.1);
+					-webkit-transform: scale(1.1);
+					-o-transform: scale(1.1);
+					-ms-transform: scale(1.1); /* IE 9 */
+					-ms-filter: "progid:DXImageTransform.Microsoft.Matrix(M11=1.5, M12=0, M21=0, M22=1.5, SizingMethod='auto expand')"; /* IE8 */
+					filter: progid:DXImageTransform.Microsoft.Matrix(M11=1.5, M12=0, M21=0, M22=1.5, SizingMethod='auto expand'); /* IE6 and 7 */
+				}
+			}
 		}
 		
-		.content {
+		.header:hover {
+		
+		}
+		
+		&.side-image
+		{
+			@supports ( display: flex )
+			{
+				display: flex;
+				.title {
+					font-size: 35px;
+				}
+				.header {
+					min-width: 50%;
+					border-bottom: none;
+					border-right: 4px solid orange;
+				}
+				
+			}
+		}
+		&.side-image-rev
+		{
+			@supports ( display: flex )
+			{
+				display: flex;
+				flex-direction: row-reverse;
+				.title {
+					font-size: 35px;
+				}
+				.header {
+					min-width: 50%;
+					border-bottom: none;
+					border-left: 4px solid orange;
+				}
+				
+			}
+		}
+		
+		.content
+		{
+			float: right;
+			height: 100%;
+			position: relative;
+			padding-left: 15px;
+			padding-right: 15px;
+			padding-top: 15px;
 			
 			.info {
 			
 			}
 			
 			.title {
-			
+				color: black;
+				padding: 0;
+				margin: 0;
 			}
 			
 			.teaser {
