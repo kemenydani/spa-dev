563fc0dbb8ce4fd681bd63a7c97f44d296840a35
Dani <kemenydani93@gmail.com>
2018-03-19 20:01:20 +0100

    progress

--- app/routes/init.php
+++ app/routes/init.php
@@ -9,7 +9,7 @@ $configuration = [
 
 $c = new \Slim\Container($configuration);
 $app = new \Slim\App($c);
-$app->config('debug', true);
+
 require 'home.php';
 require 'api.php';
 require 'upload.php';
