5e938542780041eceef0a19e60a65ea7012be8cd
Dani <kemenydani93@gmail.com>
2018-03-19 19:41:17 +0100

    progress

--- public/index.php
+++ public/index.php
@@ -7,11 +7,10 @@ define('__ROOT__', realpath(dirname(__FILE__) . '/..'));
 define('__PUBDIR__', __ROOT__ . '/public/' );
 define('__APPDIR__', __ROOT__ . '/app/' );
 define('__DEBUG__', true );
-echo 1;
-die();
-require_once '../vendor/autoload.php';
-require_once '../app/assets/helpers.php';
-require_once '../app/routes/init.php';
+
+require_once __ROOT__   . '/vendor/autoload.php';
+require_once __APPDIR__ . '/assets/helpers.php';
+require_once __APPDIR__ . '/routes/init.php';
 
 var_dump(get_post_max_size_bytes());
 
