a5756e02ef832a5834870a08794d4f92b2d6a83c
kemenydani <kemenydani93@gmail.com>
2018-04-01 12:20:20 +0200

    progress

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -37,9 +37,39 @@ class AuthController extends ViewController
             return $response->withStatus(404, 'Could not find user.');
 
         if(!$User->login( $password, $remember))
-            return $response->withStatus(404, 'Authorization failed.');
+            return $response->withJson(['success' => false])->withStatus(404, 'Authorization failed.');
 
-        return $response->withJson($User->getProperties());
+        return $response->withJson(['success' => true])->withStatus(200);
+    }
+
+
+    public static function validateUsername( $username )
+    {
+        if(strlen($username) === 0) return 'This field is required!';
+        if(strlen($username) <   3) return 'The username must be minimum 3 characters long!';
+        if(is_numeric($username))   return 'The username must contain letters.';
+        if(User::find($username, 'username')) return 'The username: '.$username.' already exists';
+        return null;
+    }
+
+    public static function validateEmail( $email )
+    {
+        if(strlen($email) === 0) return 'This field is required!';
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) return 'The format of the email address is not valid!';
+        if(User::find($email, 'email')) return 'The email: '.$email.' already exists';
+        return null;
+    }
+
+    public static function validatePassword( $password )
+    {
+        if(strlen($password) === 0) return 'This field is required!';
+        if(strlen($password) <   6) return 'The password must be minimum 6 characters long!';
+        $format = 'The password must contain at least ';
+        $msgs = [];
+        if(!preg_match('/[A-Z]/', $password)) $msgs[] = 'one uppercase letter';
+        if(!preg_match('/[0-9]/', $password)) $msgs[] = 'one number';
+        if(count($msgs) > 0) return $format . implode(' and ', $msgs);
+        return null;
     }
 
     public function postRegister ( Request $request, Response $response )
@@ -50,8 +80,13 @@ class AuthController extends ViewController
 
         $errors = [];
 
-        if(User::find($username, 'username')) $errors['username'] = 'Username already exists';
-        if(User::find($email, 'email')) $errors['email'] = 'Email already exists';
+        $usernameErr = self::validateUsername($username);
+        $emailErr    = self::validateEmail($email);
+        $passwordErr = self::validatePassword($password);
+
+        if($usernameErr) $errors['username'] =  $usernameErr;
+        if($emailErr)    $errors['email']    =  $emailErr;
+        if($passwordErr) $errors['password'] =  $passwordErr;
 
         if(count($errors) > 0) return $response->withJson(['success' => false, 'details' => $errors ]);
 
