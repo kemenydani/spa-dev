5c8226361c4374c12b501195b6439e07be6614e0
kemenydani <kemenydani93@gmail.com>
2018-04-09 15:57:34 +0200

    progress

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -11,10 +11,12 @@ class HomeController extends ViewController
 {
     public function index ( Request $request, Response $response )
     {
+        $hlArticles = DB::instance()->getRows('SELECT * FROM _xyz_article WHERE highlighted = 1 ORDER BY date_created DESC LIMIT 2');
+        $lastArticles = DB::instance()->getRows('SELECT * FROM _xyz_article WHERE highlighted = 0 ORDER BY date_created DESC LIMIT 4');
 
-
-       $this->view->render($response, 'route.view.home.html.twig', [
-
-       ]);
+        $this->view->render($response, 'route.view.home.html.twig', [
+            'hlArticles' => $hlArticles,
+            'lastArticles' => $lastArticles
+        ]);
     }
 }
