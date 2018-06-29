97d064806ce7e49cb5c129cc72043a0f3689af9b
kemenydani <kemenydani93@gmail.com>
2018-06-29 14:39:41 +0200

    progress

--- resources/spa-admin/routes/gallery.js
+++ resources/spa-admin/routes/gallery.js
@@ -8,11 +8,6 @@ const Overview = resolve =>
 	require.ensure(['../components/views/gallery/overview.vue'], () => resolve(require('../components/views/gallery/overview.vue')))
 };
 
-const Upload = resolve =>
-{
-	require.ensure(['../components/views/gallery/upload.vue'], () => resolve(require('../components/views/gallery/upload.vue')))
-};
-
 export default
 {
     path: '/gallery',
@@ -24,14 +19,6 @@ export default
             name: 'gallery.overview',
             meta: {
                 title: 'Gallery Overview'
-            }
-        },
-        {
-            path: 'upload/:id',
-            component: Upload,
-            name: 'gallery.upload',
-            meta: {
-              title: 'Upload Gallery Images'
             }
         }
     ]
