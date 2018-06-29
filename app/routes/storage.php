8c6eeccb634c5445d914280ca7f9b578618b6743
kemenydani <kemenydani93@gmail.com>
2018-06-29 08:43:54 +0200

    progress

--- app/routes/storage.php
+++ app/routes/storage.php
@@ -3,6 +3,7 @@
 use models\Country;
 use models\SquadMember;
 use Slim\Http\Response;
+use Slim\Http\Request;
 use models\User as User;
 use models\Article as Article;
 use models\EnemyTeam as EnemyTeam;
@@ -22,39 +23,41 @@ function modelImageResponse(Response $response, $path)
 
 $app->get('/userProfilePicture/[{filename}]', function( $request, $response, $args )
 {
-    $path = User::getRealImagePath($args['filename']);
-
-    if($path) return modelImageResponse($response, $path);
-
+    if(!empty($args['filename']))
+    {
+        $path = User::getRealImagePath($args['filename']);
+        if(file_exists($path)) return modelImageResponse($response, $path);
+    }
     return modelImageResponse($response, __NOIMAGE__ . DIRECTORY_SEPARATOR . User::NO_USER_IMAGE);
 });
 
-
-
 $app->get('/article_headline/[{filename}]', function( $request, $response, $args )
 {
-    $path = Article::getRealImagePath($args['filename']);
-
-    if($path) return modelImageResponse($response, $path);
-
+    if(!empty($args['filename']))
+    {
+        $path = Article::getRealImagePath($args['filename']);
+        if(file_exists($path)) return modelImageResponse($response, $path);
+    }
     return modelImageResponse($response,__NOIMAGE__ . DIRECTORY_SEPARATOR . 'no-image-grey-cross.jpg' );
 });
 
 $app->get('/enemy_team_logo/[{filename}]', function( $request, $response, $args )
 {
-    $path = EnemyTeam::getRealImagePath($args['filename']);
-
-    if($path) return modelImageResponse($response, $path);
-
+    if(!empty($args['filename']))
+    {
+        $path = EnemyTeam::getRealImagePath($args['filename']);
+        if (file_exists($path)) return modelImageResponse($response, $path);
+    }
     return modelImageResponse($response, __NOIMAGE__ . DIRECTORY_SEPARATOR . Squad::NO_LOGO_IMAGE );
 });
 
 $app->get('/squad_logo/[{filename}]', function( $request, $response, $args )
 {
-    $path = Squad::getRealImagePath($args['filename']);
-
-    if($path !== null) return modelImageResponse($response, $path);
-
+    if(!empty($args['filename']))
+    {
+        $path = Squad::getRealImagePath($args['filename']);
+        if (file_exists($path)) return modelImageResponse($response, $path);
+    }
     return modelImageResponse($response, __NOIMAGE__ . DIRECTORY_SEPARATOR . Squad::NO_LOGO_IMAGE );
 });
 
@@ -63,27 +66,30 @@ $app->get('/squad_header_image/[{filename}]', function( $request, $response, $ar
     if(!empty($args['filename']))
     {
         $path = Squad::getRealImagePath($args['filename']);
-        if (file_exists($path)) return modelImageResponse($response, $path);
+        if(file_exists($path)) return modelImageResponse($response, $path);
     }
     return modelImageResponse($response, __NOIMAGE__ . DIRECTORY_SEPARATOR . 'no-image-grey-cross.jpg' );
 });
 
 $app->get('/squad_home_wallpaper/[{filename}]', function( $request, $response, $args )
 {
-    $path = Squad::getRealImagePath($args['filename']);
-
-    if($path) return modelImageResponse($response, $path);
-
+    if(!empty($args['filename']))
+    {
+        $path = Squad::getRealImagePath($args['filename']);
+        if ($path) return modelImageResponse($response, $path);
+    }
     return modelImageResponse($response, __NOIMAGE__ . DIRECTORY_SEPARATOR . Squad::NO_HOME_WALLPAPER);
 });
 
 
 $app->get('/squadMemberHomeAvatar/[{filename}]', function( $request, $response, $args )
 {
-	$path = SquadMember::getRealImagePath($args['filename']);
-	
-	if($path) return modelImageResponse($response, $path);
-	
+    if(!empty($args['filename']))
+    {
+        $path = SquadMember::getRealImagePath($args['filename']);
+
+        if ($path) return modelImageResponse($response, $path);
+    }
 	return modelImageResponse($response, __NOIMAGE__ . DIRECTORY_SEPARATOR . Squad::NO_HOME_WALLPAPER);
 });
 
@@ -151,4 +157,3 @@ $app->get('/requestBigFlag/[{filename}]', function( $request, $response, $args )
     }
     return modelImageResponse($response, Country::NO_BIG );
 });
-
